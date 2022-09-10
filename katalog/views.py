7363196c530a54c046330578fc8574a5884232f3
annisa27 <annisa.az@ui.ac.id>
2022-09-10 19:53:26 +0700

    integrate models n views

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Annisa Az Zahra',
+        'npm': '2106701242'
+
+    }
+    return render(request, "katalog.html", context)
