8129ff2f62402b61cd2f5a32777bfc6df64a48e2
annisa27 <annisa.az@ui.ac.id>
2022-09-28 10:47:40 +0700

    create user

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
 # Register your models here.
+user = User.objects.create_user('annisaui', 'annisa.az@ui.ac.id','annisa123')
+user.save()
