22d64d67dfaca99bfa195d57d5d9126fd2b9ca00
annisa27 <annisa.az@ui.ac.id>
2022-11-23 20:18:43 +0700

    Fix2

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -8,8 +8,8 @@ app_name = 'mywatchlist'
 
 urlpatterns = [
     path('', show_mywatchlist, name='show_mywatchlist'),
-    path('/xml/', show_xml, name='show_xml'),
-    path('/json/', show_json, name='show_json'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
     path('html/', show_html, name='show_html'),
    
 ]
