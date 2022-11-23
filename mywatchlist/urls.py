05a670cdd356e15b058ae873b9dce800ceea31d5
annisa27 <annisa.az@ui.ac.id>
2022-11-23 19:55:30 +0700

    Fix

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from mywatchlist.views import *
-
+from mywatchlist.views import show_mywatchlist
+from mywatchlist.views import show_xml
+from mywatchlist.views import show_json
+from mywatchlist.views import show_html
 
 app_name = 'mywatchlist'
 
@@ -9,5 +11,5 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('html/', show_html, name='show_html'),
-    # path('<format>', goals_by_data),
+   
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,22 +1,31 @@
-from mywatchlist.models import BarangMyWatchlist
 from django.shortcuts import render
-from django.core import serializers
-from django.http import HttpResponse
-
-
 from mywatchlist.models import BarangMyWatchlist
+from django.http import HttpResponse
+from django.core import serializers
 
 
 # TODO: Create your views here.
 def show_mywatchlist(request):
     item_mywatchlist = BarangMyWatchlist.objects.all()
+    belum_tonton = 0
+    watched = 0 
+    pesan = ""
+    for film in item_mywatchlist:
+        if (film.watched ):
+            watched += 1
+        else:
+            belum_tonton += 1
+    if (watched >= belum_tonton):
+        pesan = "Selamat, kamu sudah banyak menonton!"
+    else:
+        pesan = "Wah, kamu masih sedikit menonton!"
     context = {
         'item_mywatchlist': item_mywatchlist,
         'nama' : 'Annisa',
-        'npm' : '2106701242',
-        'message' : message()
+        "pesan" : pesan,
     }
     return render(request, 'mywatchlist.html', context)
+
 
 def show_xml(request):
     data = BarangMyWatchlist.objects.all()
@@ -25,34 +34,24 @@ def show_xml(request):
 def show_json(request):
     data = BarangMyWatchlist.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-def goals_by_data(request, format):
-    data = BarangMyWatchlist.objects.all()
-    if format == 'json':
-        return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-    elif format == 'xml':
-       return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-    else:
-        return HttpResponse('No Goals Found')
 
 def show_html(request):
-    data = BarangMyWatchlist.objects.all()
+    item_mywatchlist = BarangMyWatchlist.objects.all()
+    belum_tonton = 0
+    watched = 0 
+    pesan = ""
+    for film in item_mywatchlist:
+        if (film.watched):
+            watched += 1
+        else:
+            belum_tonton += 1
+    if (watched >= belum_tonton):
+        pesan = "Selamat, kamu sudah banyak menonton!"
+    else:
+        pesan = "Wah, kamu masih sedikit menonton!"
     context = {
-        'item_mywatchlist': data,
-        'nama' : 'Annisa',
-        'npm' : '2106701242',
-        'message' : message()
+        'item_mywatchlist': item_mywatchlist,
+        'nama' : 'Rifqi',
+        "pesan" : pesan,
     }
     return render(request, 'mywatchlist.html', context)
-
-def message():
-    watched = BarangMyWatchlist.objects.all().values("watched")
-    sudah, all_list = 0,0
-
-    for i in watched:
-        all_list += 1
-        if i['watched'] == True:
-            sudah += 1
-
-    if sudah >= all_list-sudah:
-        return "Selamat, kamu sudah banyak menonton!"
-    return "Wah, kamu masih sedikit menonton!"
